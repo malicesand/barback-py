2501a7d7198f3cca6314339d53b4326fc477be7f
malicesand <malicesand@gmail.com>
2025-10-09 18:15:56 -0500

    Clean up App.tsx and dependencies

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import CardsPanel from './components/cardsPanel.tsx';
+import CardsPanel from './components/CardsPanel.tsx';
 // import { usePythonEvents } from './usePythonEvents';
 
 
